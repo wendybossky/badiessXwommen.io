03f32c001059c9a7149af4065a52b9afb316a7bf
wendybossky <92207159+wendybossky@users.noreply.github.com>
2025-11-26 23:30:29 +0700

    baddiess
    baddiess

---
+++ templatemo_595_3d_coverflow/templatemo_595_3d_coverflow/templatemo-3d-coverflow-scripts.js
@@ -0,0 +1,54 @@
+<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgNxN5rYwAXkqVPe8QFJX_z8gBAcFhMTy4-0OvqtDHxU1G5QfbCDYLEEPFslg7pzui4jtEGS-l3Er_6ecnlMzfQWRCpbV9u4brs4uH0dsLI14Jh3wCzBwizRlwDYNExXErbI625dhRsXT0jEwhOc87n1_Z89V1gzo2VQ3akahtwXRfjgzNEXL0fxJVGJ2j_/s200/Screenshot%202025-11-24%20201340.png" style="display:none;">
+<br>
+<center> <script type="text/javascript">
+    atOptions = {
+        'key' : 'e9167c2b903bdb0ab9dde75f2ee36525',
+        'format' : 'iframe',
+        'height' : 50,
+        'width' : 320,
+        'params' : {}
+    };
+</script>
+<script type="text/javascript" src="//dandywoken.com/e9167c2b903bdb0ab9dde75f2ee36525/invoke.js"></script></center>
+<br>
+<center><script>
+(function(avt){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = avt || {};
+s.src = "\/\/frequentrequirement.com\/blX.V\/sedIGPlJ0cYcWecj\/uecmu9WuGZcUZlpk\/PyTXYN3-M_TCEbylM\/TWkntpN\/jhcGxKMlTIIdywMGAR";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})
+</script></center>
+<center><iframe frameborder="0" height="640" src="https://cdn.slicedrive.com/uYtmcUTH1.mp4" width="480"></iframe></center>
+<center><script type="text/javascript">
+    atOptions = {
+        'key' : '0bfe481548dae90ae2fca280e4abd715',
+        'format' : 'iframe',
+        'height' : 60,
+        'width' : 468,
+        'params' : {}
+    };
+</script>
+<script type="text/javascript" src="//thickmaggot.com/0bfe481548dae90ae2fca280e4abd715/invoke.js"></script></center>
+<br>
+ <div class="page-header text-center">
+                    <center><a href=""
+
+onclick="window.open('https://t.co/wVapn2zZP0')"><img alt="" border="0" width="200" data-original-height="160" data-original-width="604" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiKZq3-_vw8IJCR485TFAYuCoLG7oQQDoztkzGmDQHRWp6qWx-M4iwte7FtqytpVQAAkkS8S3r7cnlgp1YAmkxAzaJ6gcfVD7dxU8gPV_OeL2Iig0aqZkpwMxth8_TnI4jwV2-z48Kqk7GAeWs_xHCwjjr_fS8L8JKiM3nayrY1tPvG8op5OKrBeoLn9QGi/s200/Screenshot%202025-11-20%20131006.png"/></a></center></div>
+    <!-- Histats.com  START  (aync)-->
+   <!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,4551613,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4551613&101" alt="" border="0"></a></noscript>
+<!-- Histats.com  END  -->
